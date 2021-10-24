4aceed05dc113c6e469c5b1841f1783698dc50d3
Sunnie912 <wangdf92@gmail.com>
2021-10-24 12:23:11 -0700

    ii

--- plsa.py
+++ plsa.py
@@ -60,17 +60,17 @@ class Corpus(object):
 #        path.close()
         #print(self.number_of_documents)
 
-        print(self.documents_path)
-        with open(self.documents_path, 'r') as file:
-            for line in file.readlines():
-                doc = list()
-                doc.extend(line.split())
-                self.documents.append(doc)
-                self.number_of_documents += 1
-
-        # print(self.documents)
-        print(len(self.documents))
-        print(self.number_of_documents)
+#        print(self.documents_path)
+#        with open(self.documents_path, 'r') as file:
+#            for line in file.readlines():
+#                doc = list()
+#                doc.extend(line.split())
+#                self.documents.append(doc)
+#                self.number_of_documents += 1
+#
+#        # print(self.documents)
+#        print(len(self.documents))
+#        print(self.number_of_documents)
 
     def build_vocabulary(self):
         """
